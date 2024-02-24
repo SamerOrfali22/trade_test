50af02af833860a304e6afa6841f9fd4bb53ddc8
SamerOrfali22 <samerorfali2020@gmail.com>
2024-02-24 16:10:03 +0400

    change loader widget

--- lib/features/weather/presentation/pages/weather_page.dart
+++ lib/features/weather/presentation/pages/weather_page.dart
@@ -1,10 +1,12 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
 import 'package:weather_app/base/presentation/pages/base_page.dart';
 import 'package:weather_app/base/presentation/widgets/rx/base_result_future_builder.dart';
+import 'package:weather_app/base/utils/ext/build_context_ext.dart';
 import 'package:weather_app/features/app/presentation/app_background_widget.dart';
 import 'package:weather_app/features/app/presentation/viewmodels/app_viewmodel.dart';
 import 'package:weather_app/features/weather/presentation/viewmodels/weather_viewmodel.dart';
@@ -41,6 +43,13 @@ class _WeatherPageState extends BasePage<WeatherPage, WeatherViewmodel> {
                 future: viewmodel.futureWeatherModel,
                 onRetry: () => viewmodel.fetchWeatherByCoord(),
                 onError: (_) => CityNotFoundWidget(viewmodel: viewmodel),
+                onLoading: () => Padding(
+                  padding: const EdgeInsets.only(top: 100.0),
+                  child: SpinKitThreeBounce(
+                    color: context.colors.onPrimary,
+                    size: 28.0.r,
+                  ),
+                ),
                 onSuccess: (data) {
                   return Column(
                     mainAxisAlignment: MainAxisAlignment.start,
